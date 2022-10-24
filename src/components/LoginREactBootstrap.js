af3075b35e4c174f896dfe8aa9b3032cc50d29fd
d3v-tushar <d3v.tushar@gmail.com>
2022-10-25 04:27:50 +0600

    Facebook & Github Login Intrigation

--- src/components/LoginREactBootstrap.js
+++ src/components/LoginREactBootstrap.js
@@ -1,12 +1,58 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import React from 'react';
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
 import app from '../firebase/firebase.init';
 
 const auth = getAuth(app);
 
 const LoginREactBootstrap = () => {
+    const [user, setUser] = useState({});
+    const googleProvider = new GoogleAuthProvider();
+    const facebookProvider = new FacebookAuthProvider();
+    const githubProvider = new GithubAuthProvider();
+
+    // Google Sign in
+    const handleGoogleSignIn = () =>{
+        signInWithPopup(auth, googleProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user);
+            console.log(user);
+        })
+        .catch(error => {
+            console.error(error.message)
+        })
+    }
+
+    // Facebook Sign in
+    const handleFacebookSignIn = () =>{
+        signInWithPopup(auth, facebookProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user);
+            console.log(user);
+        })
+        .catch(error =>{
+            console.error(error.message)
+        })
+    }
+
+    // Github Sign in
+    const handleGithubSignIn = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then(result =>{
+            const user = result.user;
+            setUser(user)
+            console.log(user)
+        })
+        .catch(error =>{
+            console.error(error.message)
+        })
+    }
+
+    // Email-Passwoed Sign in
     const handleLogin =(event) =>{
         event.preventDefault();
         const form = event.target;
@@ -22,9 +68,19 @@ const LoginREactBootstrap = () => {
         .then(result =>{
             const user = result.user;
             console.log(user);
+            setUser(user);
         })
         .catch(error =>{
             console.error(error);
+        })
+    }
+    const handleSignOut = () =>{
+        signOut(auth)
+        .then(() =>{
+            setUser({})
+        })
+        .catch(error =>{
+            console.error(error.message)
         })
     }
     return (
@@ -43,6 +99,22 @@ const LoginREactBootstrap = () => {
 
                 <Button variant="primary" type="submit">Login</Button>
             </form>
+            <br />
+            <p>Other Sign in Options</p>
+            {
+                user.uid? <div>
+                    <button onClick={handleSignOut}>Sign Out</button>
+                    <p>{user.displayName}</p>
+                    <p><small>{user.email}</small></p>
+                </div> :
+                <div>
+                    <button onClick={handleGoogleSignIn}>Google Sign In</button>
+                    <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
+                    <button onClick={handleGithubSignIn}>Github Sign In</button>
+                </div>
+            }
+            <br />
+            <p><small>New On this website? Please <Link to='/'>Register</Link></small></p>
         </div>
     );
 };
